909c51e9051278a635dbd7ff0aa1681fcb630468
Ella Soccoli <soccoli2015@gmail.com>
2025-09-28 17:00:26 -0400

    Verifying proper reading of env vars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    
+    dotenvy::dotenv()?;
+
+    for (key, value) in env::vars() {
+        println!("{key}: {value}");
+    }
+
+    Ok()
 }
